55cde72a13b38e78cc1471bf8203dee9dafdade5
lanvalird <strazhognay@gmail.com>
2025-04-27 06:03:46 +0400

    add 'user' value in config, edit CSS

--- popups/default.js
+++ popups/default.js
@@ -1,10 +1,24 @@
 const settings = document.getElementById("settings");
 
+const welcome = document.getElementById("welcome");
+
+async function getFromStorage(id) {
+  return await chrome.storage.local.get([id]).then((r) => r[id]);
+}
+
+settings["username"].value = await getFromStorage("username");
+settings["searcher"].value = await getFromStorage("searcher");
+
 settings.addEventListener(
   "submit",
   (e) => (
     e.preventDefault(),
-    console.log(settings["searcher"].value),
-    chrome.storage.local.set({ searcher: settings["searcher"].value })
+    chrome.storage.local.set({
+      username: settings["username"].value,
+      searcher: settings["searcher"].value,
+    })
   )
 );
+
+const username = getFromStorage("username");
+welcome.textContent = `Welcome, ${(await username) || "User"}!`;
--- scripts/sw.js
+++ scripts/sw.js
@@ -2,8 +2,9 @@ chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason !== "install" && details.reason !== "update") return;
   chrome.storage.local.set({
     searcher: "google",
+    username: "User",
   });
-  console.log('Local storage is done.')
+  console.log("Local storage is done.");
 });
 
 chrome.runtime.onStartup.addListener(() => {});
