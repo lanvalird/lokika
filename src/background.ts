3bca866090548e837b23b938f9172a030b27bf57
lanvalird <strazhognay@gmail.com>
2025-08-26 23:39:31 +0400

    add biome (+format files with that)

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,11 @@
 import browser from "webextension-polyfill";
 
 browser.runtime.onInstalled.addListener((details) => {
-  if (details.reason !== "install" && details.reason !== "update") return;
-  browser.storage.local.set({
-    searcher: "google",
-    username: "User",
-    background: undefined,
-  });
-  console.log("Local storage is done.");
+	if (details.reason !== "install" && details.reason !== "update") return;
+	browser.storage.local.set({
+		searcher: "google",
+		username: "User",
+		background: undefined,
+	});
+	console.log("Local storage is done.");
 });
