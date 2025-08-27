43781950316a490d7083dbf02004a642422fa487
lanvalird <strazhognay@gmail.com>
2025-08-27 06:08:51 +0400

    add tailwind css (base)

--- src/components/search-form/index.vue
+++ src/components/search-form/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { search } from './logic/search';
+import { search } from "./logic/search";
 </script>
 
 <template>
-  <form className="form" @submit.prevent="search">
+  <form
+    class="w-full max-w-[500px] flex flex-row justify-center items-center gap-2 text-rose-800 dark:text-rose-400"
+    className="form"
+    @submit.prevent="search"
+  >
     <svg
       width="32px"
       height="32px"
@@ -19,41 +23,11 @@ import { search } from './logic/search';
         stroke-linejoin="round"
       />
     </svg>
-    <input type="search" name="search" placeholder="Search..." />
+    <input
+      class="w-full border-2 rounded-full p-2"
+      type="search"
+      name="search"
+      placeholder="Search..."
+    />
   </form>
 </template>
-
-<style scoped>
-.form {
-  max-width: 500px;
-  width: 100%;
-
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-
-  color: var(--color-text);
-}
-
-.form input {
-  width: 100%;
-  padding: 0.55rem 1rem;
-
-  border: 2px solid;
-  border-radius: 999999999px;
-
-  background-color: rgba(50, 50, 50, 0.5);
-  color: currentColor;
-
-  box-shadow: 0 0 4px 2px rgb(255, 255, 255, 0.25),
-    0 0 2px 0 rgb(60, 30, 0, 0.5);
-}
-
-.form input::placeholder {
-  font-style: italic;
-  color: rgb(250, 250, 220, 0.5);
-}
-</style>
--- src/pages/newtab/viewers/index.vue
+++ src/pages/newtab/viewers/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import SearchForm from '@/components/search-form/index.vue';
+import SearchForm from "@/components/search-form/index.vue";
 </script>
 
 <template>
-  <main>
-    <div class="heading">
+  <main
+    class="background w-full h-full flex flex-col justify-center items-center gap-5 p-5"
+  >
+    <div
+      class="flex flex-row justify-center items-center gap-2 font-bold font-['Courier_New',_Courier,_monospace]"
+    >
       <img
         alt="Lokika logo"
         class="logo"
@@ -12,40 +16,9 @@ import SearchForm from '@/components/search-form/index.vue';
         width="35"
         height="35"
       />
-      <h1>Lokika</h1>
+      <h1 class="text-3xl">Lokika</h1>
     </div>
 
     <SearchForm />
   </main>
 </template>
-
-<style scoped>
-main {
-  width: 100%;
-  height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  padding: 2rem;
-}
-
-.heading {
-  display: flex;
-  gap: 0.75rem;
-  justify-content: center;
-  align-items: center;
-
-  & > * {
-    margin-bottom: 5rem;
-
-    font-family: "Courier New", Courier, monospace;
-    font-size: 2rem;
-    line-height: 2rem;
-
-    text-align: center;
-  }
-}
-</style>
