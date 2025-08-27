6e40d3acf8d94eb803dc20b54f9403e064628bc6
lanvalird <strazhognay@gmail.com>
2025-08-27 23:12:37 +0400

    add web-hash routing

--- src/global/app/router/index.ts
+++ src/global/app/router/index.ts
@@ -1,31 +1,29 @@
 import type { RouteRecordRaw } from 'vue-router';
-
 import Index from '@/views/newtab/viewers/index.vue';
 import Settings from '@/views/settings/viewers/index.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Index, alias: '/index.html' },
+  {
+    path: '/',
+    component: Index,
+    name: 'home',
+  },
   {
     path: '/settings',
     component: Settings,
-    alias: ['/popup'],
+    name: 'settings',
+  },
+  {
+    path: '/popup',
+    component: Settings,
+    name: 'popup',
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
-});
-
-router.beforeEach((to) => {
-  if (to.path.endsWith('index.html')) return;
-
-  if (to.path.endsWith('.html'))
-    return {
-      message: '',
-      path: to.path.slice(0, -5),
-    };
 });
 
 export default router;
