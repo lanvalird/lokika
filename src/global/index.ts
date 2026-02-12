222f6484c347aeeadeca91260de0c0d0aee19035
lanvalird <strazhognay@gmail.com>
2026-02-13 03:50:57 +0400

    format files

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,3 +1,5 @@
 import browser from 'webextension-polyfill';
 export { browser };
+
 export * from './constants';
+export * from './lib';
--- src/global/lib/utils.ts
+++ src/global/lib/utils.ts
@@ -1,7 +1,7 @@
-import { browser } from "@/global";
+import { browser } from '@/global';
 
 export async function setPageBackground(querySelector: string) {
-  await browser.storage.local.get("background").then((result) => {
+  await browser.storage.local.get('background').then((result) => {
     if (result.background) {
       const element = document.querySelector(querySelector) as HTMLElement;
       element.style.backgroundImage = `url(${result.background})`;
@@ -12,5 +12,5 @@ export async function setPageBackground(querySelector: string) {
 export async function getStorageData<T>(key: string): Promise<T> {
   const storage = browser.storage.local;
   const value = storage.get(key);
-  return value.then(result => result[key]) as T;
+  return value.then((result) => result[key]) as T;
 }
